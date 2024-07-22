9e454356b7eee135e802715acf15dfe35fbc1aac
archana <archanasuresh@gmail.com>
2024-07-22 13:42:01 +0530

    first commit

---
+++ employeeproject/employeeapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class employee(models.Model):
+    name=models.CharField(max_length=100)
+    salary=models.PositiveIntegerField()
+    designation=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100)
+
+# string represent-str representation 
+def __str__(self):
+    return self.name
+
+
